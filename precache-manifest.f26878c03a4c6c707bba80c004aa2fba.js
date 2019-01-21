76e1218f1e01af35bf14baf602b8dba7fcfb0360
tayyab <tayyab.razzaq@arbisoft.com>
2019-01-21 17:32:02 +0500

    Updates

---
+++ precache-manifest.f26878c03a4c6c707bba80c004aa2fba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63049e874c7ba1b207ef",
+    "url": "/myreads/static/css/main.0267edfc.chunk.css"
+  },
+  {
+    "revision": "63049e874c7ba1b207ef",
+    "url": "/myreads/static/js/main.63049e87.chunk.js"
+  },
+  {
+    "revision": "0a8645bf1974d3db70aa",
+    "url": "/myreads/static/js/1.0a8645bf.chunk.js"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "29fa64d72966531cc08559167175d730",
+    "url": "/myreads/index.html"
+  }
+];
